1af0a579a13d9d99c4290b423d2e72285ff66d7b
210542 <210542@PC258919.cts.com>
2017-10-27 13:45:17 +0530

    Some more changes

--- HelloWorld/src/com/sample/SimpleClient.java
+++ HelloWorld/src/com/sample/SimpleClient.java
@@ -9,6 +9,6 @@ package com.sample;
  */
 public class SimpleClient {
 	public static void main(String[] args) {
-		System.out.println("Inside Main Method !");
+		System.out.println("Inside Main Method !..");
 	}
 }
