51fc7c028422314028cc91d8cc72b269225039b7
210542 <210542@PC258919.cts.com>
2017-10-27 12:57:23 +0530

    Initial Version

---
+++ HelloWorld/src/com/sample/SimpleClient.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.sample;
+
+/**
+ * @author 210542
+ *
+ */
+public class SimpleClient {
+	public static void main(String[] args) {
+		System.out.println("Inside Main Method !");
+	}
+}
